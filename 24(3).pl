a852e49cf91d07ff9841cf4f016ced9274e2560d
emrys2015 <33079067+emrys2015@users.noreply.github.com>
2017-10-31 22:25:46 -0700

    Update 24(3).pl

--- 24(3).pl
+++ 24(3).pl
@@ -12,7 +12,6 @@ operator(sqrt).
 %fac(0,1).
 %fac(N,F):- N>0,N1 is N-1,fac(N1,F1),F is N*F1.
 
-% Definition for operators
 cal(add, A, B, C) :- C = A + B.
 cal(sub, A, B, C) :- C = A - B.
 cal(mul, A, B, C) :- C = A * B.
@@ -29,17 +28,17 @@ toexp([X], [], X).
 toexp(L, [H|T], R) :-
 	integer(H),
 	toexp([H|L], T, R).
+
 toexp([X,Y|Z], [H|T], R) :-
 	operator(H),
 	cal(H, Y, X, X1),
 	toexp([X1|Z], T, R).
 
-%Evaluate Expressions, eval(Prog, Target) is true if the value of the prog is equal to the target
 eval(Prog, Target) :-
 	toexp([], Prog, R),
 	Target is R.
 
-% Use Reverse Polish Notation to enumerate all possible expressions (5
+% Use Reverse Polish Notation to enumerate all possible expression (5
 % types)
 
 mytry([A, B, C, D], [A, B, X, C, Y, D, Z], N) :- eval([A, B, X, C, Y, D, Z], N).
@@ -48,23 +47,21 @@ mytry([A, B, C, D], [A, B, C, X, D, Y, Z], N) :- eval([A, B, C, X, D, Y, Z], N).
 mytry([A, B, C, D], [A, B, C, X, Y, D, Z], N) :- eval([A, B, C, X, Y, D, Z], N).
 mytry([A, B, C, D], [A, B, C, D, X, Y, Z], N) :- eval([A, B, C, D, X, Y, Z], N).
 
-% set the target to 24, in case of decimal in div or square root, 24.0 is included.
-solve24order(Numbers, Prog) :- mytry(Numbers, Prog, 24).
-solve24order(Numbers, Prog) :- mytry(Numbers, Prog, 24.0).
+solve24(N, Prog) :- mytry(N, Prog, 24).
+solve24(N, Prog) :- mytry(N, Prog, 24.0).
 
-% lists all the results in a set list and write all the elements in that list.
-solve24(Numbers, Prog) :-
-	lists:perm(Numbers, T1),
-	solve24order(T1, Prog),
-	toexp([], Prog, E),
-	write(E),
+math24(N, Prog) :-
+	lists:perm(N, T1),
+	solve24(T1, Prog),
+	toexp([], Prog, R1),
+	write(R1),
 	nl,
 	fail.
 
 % Try:
-% ?- solve24([4,2,5,3], X).
-% ?- solve24([4,9,2,9], X).
-% ?- solve24([3,4,6,2], X).
-% ?- solve24([1,2,4,5], X).
-% ?- solve24([5,5,5,1], X).
-% ?- solve24([4,4,10,10], X).
+% ?- math24([4,2,5,3], X).
+% ?- math24([4,9,2,9], X).
+% ?- math24([3,4,6,2], X).
+% ?- math24([1,2,4,5], X).
+% ?- math24([5,5,5,1], X).
+% ?- math24([4,4,10,10], X).
